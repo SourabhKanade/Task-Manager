03171f693c1091121c818bb8bf1bf04d154d3487
Sourabh <kanadesourabh420@gmail.com>
2021-10-17 13:03:54 +0530

    simplified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import ExpenseItem from "./components/ExpenseItem";
+// import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
 
-   const data = [
+  const Items = [
     {
       id: '01',
       title: 'football',
@@ -29,12 +30,9 @@ function App() {
     },
   ];
   return (
-    <div>
-    <ExpenseItem title={data[0].title} amount={data[0].amount} date={data[0].date} />
-    <ExpenseItem title={data[1].title} amount={data[1].amount} date={data[1].date} />
-    <ExpenseItem title={data[2].title} amount={data[2].amount} date={data[2].date} />
-    <ExpenseItem title={data[3].title} amount={data[3].amount} date={data[3].date} />
-    </div>
+    <>
+      <Expenses data={Items}/>
+    </>
   );
 }
 
